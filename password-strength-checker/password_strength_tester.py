f579855cb0480a03ab4923de723c4577782c6065
Taylor Scott <tayzscott@gmail.com>
2025-03-18 05:53:01 -0700

    Update password_strength_tester.py
    bug fix?

--- password-strength-checker/password_strength_tester.py
+++ password-strength-checker/password_strength_tester.py
@@ -8,7 +8,8 @@ Args:
     pwd (string): user-entered password to be checked
 
 Returns:
-    sum (integer): password strength score. Defaults to 4.
+    strength (integer): password strength score. Defaults to 4.
+    feedback (string): feedback based on password entered. Defaults to 'Great job!'.
 """
 def check_pwd(pwd):
     # if pwd shorter than 12 letters, return 0
@@ -16,55 +17,36 @@ def check_pwd(pwd):
         return 0
     
     else:
-        sum = 4     # password strength tracker variable
+        strength = 4         # password strength tracker int
+        feedback = ''   # feedback string
         
         # check for at least one uppercase letter
         if not re.search(r'[A-Z]', pwd):
-            change_feedback(1)
-            sum -= 1
+            feedback += 'Please add an uppercase letter.\n'
+            strength -= 1
             
         # check for at least one lowercase letter
         if not re.search(r'[a-z]', pwd):
-            change_feedback(2)
-            sum -= 1
+            feedback += 'Please add a lowercase letter.\n'
+            strength -= 1
             
         # check for at least one digit
         if not re.search(r'\d', pwd):
-            change_feedback(3)
-            sum -= 1
+            feedback += 'Please add a number.\n'
+            strength -= 1
             
         # check for at least one special character
         if not re.search(r'[!@#$%^&*(),.?":{}|<>]', pwd):
-            change_feedback(4)
-            sum -= 1
+            feedback += 'Please add a special character.\n'
+            strength -= 1
             
-        return sum
-    
-    
-"""gives the user feedback based on the errors found in the entered password
-Args:
-    errcode (integer): error code as determined in check_pwd
-
-Returns:
-    feedback (string): feedback based on password entered. Defaults to 'Great job!'.
-"""
-def change_feedback(errcode):
-    if errcode == 1:
-        feedback += 'Please add an uppercase letter.\n'
-    if errcode == 2:
-        feedback += 'Please add a lowercase letter.\n'
-    if errcode == 3:
-        feedback += 'Please add a number.\n'
-    if errcode == 4:
-        feedback += 'Please add a special character.\n'
-    
-    if feedback == '':
-        return 'Great job!'
-    return feedback
+        if feedback == '':
+            feedback == 'Great job!'
+        
+        return strength, feedback
     
     
 # main program
 password = input('Enter your password: ')
-score = check_pwd(password)
-print(f'Your score is {score}.')
-print(change_feedback)
+score, str = check_pwd(password)
+print(f'Your score is {score}.\n{str}')
